951d2df5f3e64eeedf50d2fe94bfff19b2e63e47
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-04-26 13:38:20 -0600

    preparing to pull images

--- portfolio.js
+++ portfolio.js
@@ -28,6 +28,33 @@ $(document).ready(function () {
         $(this).css("background", "rgba(0, 0, 0, 0.5)");
       });
 
-    
+    // var imgTag = '<img class="card-img rounded-0 img-fluid p-0" src="images/' + imgName + '.jpg"></a>'
+
+      $(".featured-sea").hover(function(){
+        var newText = this.name;
+        $(this).addClass("featured-text");
+        $(this).html(newText);
+        }, function(){
+        $(this).removeClass("featured-text");
+        $(this).html('<a class="featured-sea" name="Hangman-atee: Word Guess Game" href="https://lisaswindler.github.io/Word-Guess-Game/"target="_blank"><img class="card-img rounded-0 img-fluid p-0" src="images/sea-turtle.jpg"></a>');
+      });
+
+      $(".featured-train").hover(function(){
+        var newText = this.name;
+        $(this).addClass("featured-text");
+        $(this).html(newText);
+        }, function(){
+        $(this).removeClass("featured-text");
+        $(this).html('<a class="featured-train" name="Train Time" href="https://lisaswindler.github.io/train-time"target="_blank"><img class="featured-img card-img rounded-0 img-fluid p-0" src="images/hogwarts_express.jpg"></a>');
+      });
+
+      $(".featured-plant").hover(function(){
+        var newText = this.name;
+        $(this).addClass("featured-text");
+        $(this).html(newText);
+        }, function(){
+        $(this).removeClass("featured-text");
+        $(this).html('<a class="featured-plant" name="Hike & Forage Utah" href="https://lindseypaluso.github.io/project1/"target="_blank"><img class="featured-img card-img rounded-0 img-fluid p-0" src="images/fiddlehead.jpg"></a>');
+      });
 
     });
